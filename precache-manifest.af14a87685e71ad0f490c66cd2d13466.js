7b7cfd01691001f11511b159bb6ac16f763418ba
pradhyumn14 <iampradhyumn@gmail.com>
2022-04-26 22:43:55 +0530

    Updates

---
+++ precache-manifest.af14a87685e71ad0f490c66cd2d13466.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1be2a2459d6dc6589ecdc6bede5083f0",
+    "url": "/spacex-assignment/index.html"
+  },
+  {
+    "revision": "4e48525741d02d5fd9bf",
+    "url": "/spacex-assignment/static/css/2.b8d16a07.chunk.css"
+  },
+  {
+    "revision": "2db495ff14ed4e151f3f",
+    "url": "/spacex-assignment/static/css/main.e141296e.chunk.css"
+  },
+  {
+    "revision": "4e48525741d02d5fd9bf",
+    "url": "/spacex-assignment/static/js/2.6cdbf0dc.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/spacex-assignment/static/js/2.6cdbf0dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2db495ff14ed4e151f3f",
+    "url": "/spacex-assignment/static/js/main.d4768a2b.chunk.js"
+  },
+  {
+    "revision": "496c463ce50cfad8c6c8",
+    "url": "/spacex-assignment/static/js/runtime-main.64c4f37c.js"
+  },
+  {
+    "revision": "c537d7d180515a2d14e723821dcf5097",
+    "url": "/spacex-assignment/static/media/loadRocket.c537d7d1.gif"
+  }
+]);
